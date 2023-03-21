14c80fd52ecbedbac89f8101efcce6bc3bc936a3
pedro-henrique2 <pedroworks21@gmail.com>
2023-03-21 20:09:13 -0300

    calculator-in-console

---
+++ calculator-console/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("--------------------------------------------------------------------------------");
+        Console.WriteLine("Bem Vindo a calculadora!");
+        Console.WriteLine("Por favor digite um numero, seguido de um operador (+, -, *, /), e outro numero");
+        Console.WriteLine("Por exemplo: 7 + 3");
+        Console.WriteLine("--------------------------------------------------------------------------------");
+
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+
+        if (parts.Length != 3)
+        {
+            Console.WriteLine("Entrada invalida digite novamente.");
+            return;
+        }
+
+        double num1, num2;
+
+        if (!double.TryParse(parts[0], out num1) || !double.TryParse(parts[2], out num2))
+        {
+            Console.WriteLine("Numero invalido, por favor tente novamente.");
+            return;
+        }
+
+        double result;
+
+        switch (parts[1])
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Não é possivel dividir por 0.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Operador invalido, por favor tente novamente.");
+                return;
+        }
+
+        Console.WriteLine("O resultado é: " + result);
+    }
+}
